54e5fe258b9e1d223228e4950b37961a7ee17abd
[asifneyaz] <asifneyaz@gmail.com>
2019-06-13 16:57:12 -0700

    few changes

--- c1_2.py
+++ c1_2.py
@@ -8,3 +8,35 @@ record2 = ('289-283-3231', '898-564-9876', 'Dave')
 print(*phone_num2, name2)
 print(phone_num2)
 
+# example.py
+#
+# Unpacking of tagged tuples of varying sizes
+
+records = [
+     ('foo', 1, 2),
+     ('bar', 'hello'),
+     ('foo', 3, 4),
+]
+
+def do_foo(x,y):
+    print('foo', x, y)
+
+def do_bar(s):
+    print('bar', s)
+
+for tag, *args in records:
+    if tag == 'foo':
+        do_foo(*args)
+    elif tag == 'bar':
+        do_bar(*args)
+
+
+fruits = ['mango', 'jackfruit']
+for iter in fruits:
+    print(iter)
+
+line = 'fjdsjfk:ufdgs:/adsjf/dshjf:fdhjs'
+s1, *s2, s3 = line.split(':')
+
+
+
---
+++ c1_3.py
@@ -0,0 +1,10 @@
+import collections
+from collections import deque
+
+d = deque('hello')
+print(d)
+d.append(4)
+print(d)
+
+
+
