816bbcc878409d082f0acfe11afcf2ea0e1317fe
Asif Neyaz Azeez <tech@summit.lan>
2019-10-07 19:34:13 -0700

    linkedlist

--- linkedlist/linked_list.py
+++ linkedlist/linked_list.py
@@ -1,69 +1,232 @@
-# A simple Python program to introduce a linked list 
-  
-# Node class 
-class Node: 
-  
-    # Function to initialise the node object 
-    def __init__(self, data): 
-        self.data = data  # Assign data 
-        self.next = None  # Initialize next as null 
-  
-  
-# Linked List class contains a Node object 
-class LinkedList: 
-  
-    # Function to initialize head 
-    def __init__(self): 
+
+# Node of a Singly Linked List
+class Node:
+    # constructor
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+         # method for setting the data field of the node    
+    def set_data(self, data):
+        self.data = data
+    # method for getting the data field of the node   
+    def get_data(self):
+        return self.data
+      # method for setting the next field of the node
+    def set_next(self, next):
+        self.next = next
+       # method for getting the next field of the node    
+    def get_next(self):
+        return self.next
+    # returns true if the node points to another node
+    def has_next(self):
+            return self.next != None
+
+# class for defining a linked list   
+class LinkedList(object):
+     
+    # initializing a list
+    def __init__(self):
+        self.length = 0
         self.head = None
-  
-  
-# Code execution starts here 
-if __name__=='__main__': 
-  
-    # Start with the empty list 
-    llist = LinkedList() 
-  
-    llist.head = Node(1) 
-    second = Node(2) 
-    third = Node(3) 
-  
-    ''' 
-    Three nodes have been created. 
-    We have references to these three blocks as head, 
-    second and third 
-  
-    llist.head        second              third 
-         |                |                  | 
-         |                |                  | 
-    +----+------+     +----+------+     +----+------+ 
-    | 1  | None |     | 2  | None |     |  3 | None | 
-    +----+------+     +----+------+     +----+------+ 
-    '''
-  
-    llist.head.next = second; # Link first node with second  
-  
-    ''' 
-    Now next of first Node refers to second.  So they 
-    both are linked. 
-  
-    llist.head        second              third 
-         |                |                  | 
-         |                |                  | 
-    +----+------+     +----+------+     +----+------+ 
-    | 1  |  o-------->| 2  | null |     |  3 | null | 
-    +----+------+     +----+------+     +----+------+  
-    '''
-  
-    second.next = third; # Link second node with the third node 
-  
-    ''' 
-    Now next of second Node refers to third.  So all three 
-    nodes are linked. 
-  
-    llist.head        second              third 
-         |                |                  | 
-         |                |                  | 
-    +----+------+     +----+------+     +----+------+ 
-    | 1  |  o-------->| 2  |  o-------->|  3 | null | 
-    +----+------+     +----+------+     +----+------+  
-    '''
+         
+    # method to add a node in the linked list
+    def addNode(self, node):
+        if self.length == 0:
+            self.addBeg(node)
+        else:
+            self.addLast(node)
+             
+         
+    # method to add a node at the beginning of the list with a data   
+    def addBeg(self, node):
+        newNode = node
+        newNode.next = self.head
+        self.head = newNode
+        self.length += 1
+         
+    # method to add a node after the node having the data=data. The data of the new node is value2
+    def addAfterValue(self, data, node):
+        newNode = node
+        currentnode = self.head
+         
+        while currentnode.next != None or currentnode.data != data:
+            if currentnode.data == data:
+                newNode.next = currentnode.next
+                currentnode.next = newNode
+                self.length = self.length + 1
+                return
+            else:
+                currentnode = currentnode.next
+                 
+        print("The data provided is not present")
+                 
+    # method to add a node at a particular position
+    def addAtPos(self, pos, node):
+        count = 0
+        currentnode = self.head
+        previousnode = self.head
+         
+        if pos > self.length or pos < 0:
+            print("The position does not exist. Please enter a valid position")
+        else:
+            while currentnode.next != None or count < pos:
+                count = count + 1
+                if count == pos:
+                    previousnode.next = node
+                    node.next = currentnode
+                    self.length += 1
+                    return
+                     
+                else:
+                    previousnode = currentnode
+                    currentnode = currentnode.next
+         
+         
+                 
+    # method to add a node at the end of a list
+    def addLast(self, node):
+        currentnode = self.head
+         
+        while currentnode.next != None:
+            currentnode = currentnode.next
+ 
+        newNode = node
+        newNode.next = None
+        currentnode.next = newNode
+        self.length = self.length + 1
+     
+     
+    # method to delete the first node of the linked list
+    def deleteBeg(self):
+        if self.length == 0:
+            print("The list is empty")
+        else:
+            self.head = self.head.next
+            self.length -= 1
+     
+    # method to delete the last node of the linked list
+    def deleteLast(self):
+         
+        if self.length == 0:
+            print("The list is empty")
+        else:
+            currentnode = self.head
+            previousnode = self.head
+             
+            while currentnode.next != None:
+                previousnode = currentnode
+                currentnode = currentnode.next
+                 
+            previousnode.next = None
+             
+            self.length -= 1
+             
+         
+    # method to delete a node after the node having the given data
+    def deleteValue(self, data):
+        currentnode = self.head
+        previousnode = self.head
+         
+        while currentnode.next != None or currentnode.data != data:
+            if currentnode.data == data:
+                previousnode.next = currentnode.next
+                self.length -= 1
+                return
+                    
+            else:
+                previousnode = currentnode
+                currentnode = currentnode.next
+                 
+        print("The data provided is not present")
+                 
+         
+         
+    # method to delete a node at a particular position
+    def deleteAtPos(self, pos):
+        count = 0
+        currentnode = self.head
+        previousnode = self.head
+ 
+        if pos > self.length or pos < 0:
+            print("The position does not exist. Please enter a valid position")
+        # to deletle the first position of the linkedlist
+        elif pos == 1:
+            self.delete_beg()
+            self.length -= 1
+        else:        
+            while currentnode.next != None or count < pos:
+                count = count + 1
+                if count == pos:
+                    previousnode.next = currentnode.next
+                    self.length -= 1
+                    return
+                else:
+                    previousnode = currentnode
+                    currentnode = currentnode.next
+                     
+             
+ 
+     
+    # returns the length of the list        
+    def getLength(self):
+        return self.length
+     
+    # returns the first element of the list
+    def getFirst(self):
+        if self.length == 0:
+            print("The list is empty")
+        else:
+            return self.head.data
+     
+    # returns the last element of the list
+    def getLast(self):
+         
+        if self.length == 0:
+            print("The list is empty")
+        else:
+            currentnode = self.head
+             
+            while currentnode.next != None:
+                currentnode = currentnode.next
+                 
+            return currentnode.data
+     
+    # returns node at any position
+    def getAtPos(self, pos):
+        count = 0
+        currentnode = self.head
+         
+        if pos > self.length or pos < 0:
+            print("The position does not exist. Please enter a valid position")
+        else:
+            while currentnode.next != None or count < pos:
+                count = count + 1
+                if count == pos:
+                    return currentnode.data
+                else:
+                    currentnode = currentnode.next
+ 
+                     
+    # method to print the whole list
+    def print_list(self):
+        nodeList = []
+        currentnode = self.head
+        while currentnode != None:
+            nodeList.append(currentnode.data)
+            currentnode = currentnode.next 
+             
+        print(nodeList)
+
+node1 = Node(1)
+node2 = Node(2)
+node3 = Node(3)
+node4 = Node(4)
+node5 = Node(5)
+
+ll = LinkedList()
+ll.addNode(node1)
+ll.addNode(node2)
+ll.addNode(node3)
+ll.addNode(node4)
+ll.addNode(node5)
+ll.print_list()
